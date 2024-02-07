66eddc6ab6847bde3d1e8e403fb223eba8220268
Reycecar <Reycerxa@gmail.com>
2024-02-07 09:41:07 -0500

    Updated fisher bot for discord update

--- FisherBot.py
+++ FisherBot.py
@@ -1,44 +1,55 @@
 '''
 @Author Reyce Salisbury
 3/4/2022
+
+TODO:
+Fix other commands outside of go_fish_click()
+Add multiprocessing or multithreading to detect multiple user inputs
+    - might work by adding second controller??
+    - Thread Queue?
+
 '''
 
 from multiprocessing import Event
-from pynput.keyboard import *
+import pynput
 import os
 import time
+import random
 
-keyboard = Controller()
+keyboard = pynput.keyboard.Controller()
+mouse = pynput.mouse.Controller()
 
-RUNS = 5
-COOLDOWN = 2.8
-BAIT = "Fish"
+RUNS = 1000000
+COOLDOWN = 3
+BAIT = "Magic Bait"
 BAITNUM = 50
-                    
 
-#Types %f (fishing), is interrupted by pressing end
-def go_fish():
+#Types %f (fishing)
+def go_fish_type():
     for _ in range(RUNS):
         txt = "%f"
         typer(txt)
-        time.sleep(COOLDOWN)
+        # Sleep for cooldown time, but subtract the time it takes to type
+        time.sleep(COOLDOWN - (len(txt)*.1))
 
-#buys BAITNUM of BAIT when end is pressed after program is run
-def get_bait():
-    txt = "%buy " + BAIT + " " + str(BAITNUM)
-    typer(txt)
+# Clicks Fishing button, avoids captcha failure clause
+def go_fish_click():
+    while True:
+        clicker()
+        # Add variance (up to 1 second) in time between clicks, helps avoid captcha detection
+        time.sleep(COOLDOWN + round(random.random(), 1) + 1.5) # Add buffer time to cooldown timer to counteract lag
 
-#called when insert key is pressed after program is run
+# **BROKEN BY UPDATE** called when insert key is pressed after program is run
 def sell():
-    txt = "%sell all"
+    txt = "/sell all"
     typer(txt)
 
-#function for pressing enter
+# Function for pressing enter
 def enter():
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
+    keyboard.press(pynput.keyboard.Key.enter)
+    keyboard.release(pynput.keyboard.Key.enter)
 
-#Types text to wherever your curser is
+# Types text to wherever your curser is
 def typer(text):
     for i in text:
         keyboard.press(i)
@@ -46,20 +57,21 @@ def typer(text):
         time.sleep(0.1)
     enter()
 
+# User must hover mouse over button
+def clicker():
+    mouse.press(pynput.mouse.Button.left)
+    mouse.release(pynput.mouse.Button.left)
+
 #Runs funtions based on Key Pressed
 def on_press(key):
-    if key == Key.num_lock:
-        go_fish()
-    elif key == Key.insert:
-        sell()
-    elif key == Key.end:
-        get_bait()
-    elif key == Key.esc:
+    if key == pynput.keyboard.Key.alt_l:  # clicker fishing
+        go_fish_click()
+    elif key == pynput.keyboard.Key.esc:
         os.system('taskkill /IM "Python.exe" /F')
 
 #Main, Starts the listener
 def main():
-    with Listener(on_press=on_press, on_release=None) as listener:
+    with pynput.keyboard.Listener(on_press=on_press, on_release=None) as listener:
         while True:
             listener.run()
 
